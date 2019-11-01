69cc860b0c8ef235595c3e395e81952b9abd1a2c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-31 22:38:37 -0600

    Test errorMsg reducer, returns default state

--- src/reducerTesting/errorReducer.test.js
+++ src/reducerTesting/errorReducer.test.js
@@ -1,2 +1,10 @@
 import { errorMsg } from '../reducers/errorReducer';
 
+describe('errorMsg', () => {
+    it('should return default state', () => {
+      const expected = '';
+      const result = errorMsg(undefined, '');
+
+      expect(result).toEqual(expected);
+    })
+})
