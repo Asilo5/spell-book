9cc0bf0ba3069cb59fac2c0da6cadf5b00c3a8b8
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-31 19:20:06 -0600

    Add reducer files of loadingReducer and nameREducer

--- src/Containers/HouseForm/HouseForm.js
+++ src/Containers/HouseForm/HouseForm.js
@@ -40,10 +40,10 @@ render() {
                          name='wizardName' 
                          value={this.state.wizardName} 
                          onChange={this.handleChange} />
-                    <img className='name-banner' src='https://www.pinclipart.com/picdir/big/10-101144_vintage-banner-vector-png-theveliger-clipart-vintage-banner.png' alt='banner name image' />
+                    <img className='name-banner' src='https://www.pinclipart.com/picdir/big/10-101144_vintage-banner-vector-png-theveliger-clipart-vintage-banner.png' alt='banner name' />
                   </div>
                   <div onClick={(e) => this.foundHouse(e)}>
-                    <img className='sorting-button' src='https://cdn.shopify.com/s/files/1/0221/1146/products/Sorting_Hat_pin_badge_scaled_grande.png?v=1551715337' alt='sorting hat image' />
+                    <img className='sorting-button' src='https://cdn.shopify.com/s/files/1/0221/1146/products/Sorting_Hat_pin_badge_scaled_grande.png?v=1551715337' alt='sorting hat' />
                     <h2 className='submit-text'>Enter</h2>
                   </div>
               </form>
