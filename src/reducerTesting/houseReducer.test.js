28ccfe48c040ae55daa319c21608402fd276724d
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-31 22:58:14 -0600

    Test if userHouse reducer changed state, test passes

--- src/reducerTesting/houseReducer.test.js
+++ src/reducerTesting/houseReducer.test.js
@@ -6,5 +6,16 @@ describe('userHouse', () => {
 
        const result = userHouse(undefined, '');
        expect(result).toEqual(expected);
+    }); 
+
+    it('should return house as state', () => {
+        const action = {
+            type: 'GIVEN_HOUSE',
+            house: 'Hufflepuff'
+        }
+        const expected = 'Hufflepuff';
+        
+        const result = userHouse(undefined, action);
+        expect(result).toEqual(expected);
     })
 })
