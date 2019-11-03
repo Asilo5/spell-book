9830e387688a9747c105277b0af7e9207b8c53c9
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-02 18:24:41 -0600

    Add background image to spell

--- src/Containers/SpellBook/SpellBook.js
+++ src/Containers/SpellBook/SpellBook.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const SpellBook = ({id, spell, type, effect}) => {
-    console.log(spell);
   return (
       <section id={id}>
-          <h2>{spell}</h2>
-          <h3>{type}</h3>
-          <p>{effect}</p>
+          <div>
+            <img className='favourite-wand' src='https://gamepress.gg/wizardsunite/sites/wizardsunite/files/2019-04/Luna%20Lovegood-foundable_0.png' alt='luna lovegood wand to favourite spell card' />
+            <h2>{spell}</h2>
+            <h3>{type}</h3>
+            <p>{effect}</p>
+          </div>
+          <img className='paper-img' src='https://www.artsclip.com/background/XZHaFAgbeP-vintage-paper-scroll-for-short-news-slides.png' alt='background paper for spells' />
       </section>
   )
 }
