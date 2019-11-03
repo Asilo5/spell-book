104e6f4007cebe9b3353b2b4baf8f9fef3867a59
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-02 18:11:32 -0600

    Append spells to page

--- src/Containers/SpellBook/SpellBook.js
+++ src/Containers/SpellBook/SpellBook.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const SpellBook = () => {
+const SpellBook = ({id, spell, type, effect}) => {
+    console.log(spell);
   return (
-      <section id='id'>
-          <h2>Spell</h2>
-          <h3>Type</h3>
-          <p>Effect</p>
+      <section id={id}>
+          <h2>{spell}</h2>
+          <h3>{type}</h3>
+          <p>{effect}</p>
       </section>
   )
 }
