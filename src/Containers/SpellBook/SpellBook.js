11a95e311106377fcc319d341e6395907924c4c6
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-02 19:21:54 -0600

    Add font styling to spells

--- src/Containers/SpellBook/SpellBook.js
+++ src/Containers/SpellBook/SpellBook.js
@@ -7,9 +7,9 @@ const SpellBook = ({id, spell, type, effect}) => {
           <div className='spell-container'>
             <img className='favourite-wand' src='https://gamepress.gg/wizardsunite/sites/wizardsunite/files/2019-04/Luna%20Lovegood-foundable_0.png' alt='luna lovegood wand to favourite spell card' />
             <div className='spell-info'>
-              <h2>{spell}</h2>
-              <h3>{type}</h3>
-              <p>{effect}</p>
+              <h3>{spell}</h3>
+              <h4>{type}</h4>
+              <p>This {type} {effect}</p>
             </div>
           </div>
           <img className='paper-img' src='https://www.artsclip.com/background/XZHaFAgbeP-vintage-paper-scroll-for-short-news-slides.png' alt='background paper for spells' />
