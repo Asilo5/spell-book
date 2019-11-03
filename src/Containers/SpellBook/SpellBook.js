3126d7fb48d07a131d26db0a68439b72cb35ad00
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-02 18:58:29 -0600

    Position spell correctly on page

--- src/Containers/SpellBook/SpellBook.js
+++ src/Containers/SpellBook/SpellBook.js
@@ -3,12 +3,14 @@ import './SpellBook.css';
 
 const SpellBook = ({id, spell, type, effect}) => {
   return (
-      <section id={id}>
-          <div className='spell-info'>
+      <section className='spells' id={id}>
+          <div className='spell-container'>
             <img className='favourite-wand' src='https://gamepress.gg/wizardsunite/sites/wizardsunite/files/2019-04/Luna%20Lovegood-foundable_0.png' alt='luna lovegood wand to favourite spell card' />
-            <h2>{spell}</h2>
-            <h3>{type}</h3>
-            <p>{effect}</p>
+            <div className='spell-info'>
+              <h2>{spell}</h2>
+              <h3>{type}</h3>
+              <p>{effect}</p>
+            </div>
           </div>
           <img className='paper-img' src='https://www.artsclip.com/background/XZHaFAgbeP-vintage-paper-scroll-for-short-news-slides.png' alt='background paper for spells' />
       </section>
