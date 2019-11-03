20e350de1fbf7216ee36535bd381cc8d38b23ef0
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-02 21:58:21 -0600

    Add method to be able to find spells

--- src/Containers/SpellsForm/SpellsForm.js
+++ src/Containers/SpellsForm/SpellsForm.js
@@ -4,12 +4,28 @@ class SpellsForm extends Component {
   constructor() {
     super();
     this.state = {
-      spell: ''
+      chosenSpell: ''
     }
   }
 
   handleChange = (e) => {
-    this.setState({ [e.target.name] : e.target.value })
+    this.setState({ [e.target.name] : e.target.value });
+  }
+
+  findSpell = (e) => {
+    e.preventDefault();
+    const { chosenSpell } = this.state;
+
+    let chosenOne = spells.filter((spell) => {
+        let lowerCaseSpells = spell.spell.toLowerCase();
+        return lowerCaseSpells.includes(chosenSpell.toLowerCase());
+      });
+
+    this.resetInputs();
+  }
+
+  resetInputs = () => {
+    this.setState({ spell : '' });
   }
 
   render() {
@@ -18,8 +34,8 @@ class SpellsForm extends Component {
             <input className='search-spells'
                     placeholder='Search for Spell'
                     type='text'
-                    name='spell' 
-                    value={this.state.spell}
+                    name='chosenSpell' 
+                    value={this.state.chosenSpell}
                     onChange={this.handleChange}/>
             <button>Accio!</button>
         </form>
