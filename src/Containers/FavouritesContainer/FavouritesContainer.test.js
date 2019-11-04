c2c9003e38eb5e73412517c830e55ce72cb7f8ad
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-04 16:04:27 -0700

    Update snapshots and write new tests for favourites container

---
+++ src/Containers/FavouritesContainer/FavouritesContainer.test.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { FavouritesContainer, mapStateToProps } from './FavouritesContainer';
+import { shallow } from 'enzyme';
+
+describe('FavouritesContainer', () => {
+
+    describe('favourites', () => {
+
+       let mockFavouriteSpell = [
+        { _id: '5b74f3653228320021ab628b',
+        spell: 'Lumos',
+        type: 'Spell',
+        effect: 'creates light at wand tip',
+        __v: 0 }
+       ];
+
+       let mockSpellBook = [
+        { _id: '5b74f3583228320021ab628a',
+        spell: 'Locomotor Mortis',
+        type: 'Curse',
+        effect: 'locks opponent\'s legs',
+        __v: 0 },
+      { _id: '5b74f3653228320021ab628b',
+        spell: 'Lumos',
+        type: 'Spell',
+        effect: 'creates light at wand tip',
+        __v: 0 } 
+       ];
+
+       let wrapper = shallow( <FavouritesContainer 
+               favouriteSpells={mockFavouriteSpell}
+                spellBook={mockSpellBook}
+       /> )
+       
+      it('should match snapshot with all information passing in correctly', () => {
+         expect(wrapper).toMatchSnapshot();
+      })
+    })
+
+    describe('mapStateToProps', () => {
+        it('should return a spell object', () => {
+            const mockState = {
+               "favouriteSpells": [   
+                { _id: '5b74f3653228320021ab628b',
+                   spell: 'Lumos',
+                   type: 'Spell',
+                   effect: 'creates light at wand tip',
+                   __v: 0 }
+                ],
+               "spellBook": [
+                  { _id: '5b74f3653228320021ab628b',
+                  spell: 'Lumos',
+                  type: 'Spell',
+                  effect: 'creates light at wand tip',
+                __v: 0 }
+               ],
+            }
+            const expected = {
+               "favouriteSpells": [
+                { _id: '5b74f3653228320021ab628b',
+                spell: 'Lumos',
+                type: 'Spell',
+                effect: 'creates light at wand tip',
+                __v: 0 }
+               ],
+                "spellBook": [
+                    { _id: '5b74f3653228320021ab628b',
+                   spell: 'Lumos',
+                   type: 'Spell',
+                   effect: 'creates light at wand tip',
+                   __v: 0 }
+                ],
+            }
+            const mappedProps = mapStateToProps(mockState);
+            expect(mappedProps).toEqual(expected);
+          });
+    })
+
+})
--- src/reducers/favouritesReducer.js
+++ src/reducers/favouritesReducer.js
@@ -7,4 +7,4 @@ export const favouriteSpells = ( state = [], action ) => {
         default:
           return state;
     }  
-}
+} 
