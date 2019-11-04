34b21334f2dca8c43621238f6c111d3534c12ab9
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-04 11:32:45 -0700

    Add Route to favourites in App

--- src/Containers/FavouritesContainer/FavouritesContainer.js
+++ src/Containers/FavouritesContainer/FavouritesContainer.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Favourite from '../Favourite/Favourite';
 
-const FavouritesContainer = ({favouriteSpells}) => {
-
+const FavouritesContainer = ({favouriteSpells, spellBook}) => {
+   const favSpell = favouriteSpells.map((id) => {
+      if(spellBook._id.includes(id)) {
+          console.log(spellBook);
+        //   return <Favourite key={id} />
+      }
+   })
     return (
       <section>
-
+         {/* {favSpell} */}
       </section>
     )
 }
 
-export const mapStateToProps = ({favouriteSpells}) => ({
-    favouriteSpells
+export const mapStateToProps = ({favouriteSpells, spellBook}) => ({
+    favouriteSpells,
+    spellBook
 })
 
 export default connect(mapStateToProps)(FavouritesContainer);
