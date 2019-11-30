7a2e544bcc3614de65720e1d0508695604b5f726
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-29 18:43:13 -0700

    Add method in Spells Container that filters through to find spells

--- src/Containers/SpellsContainer/SpellsContainer.js
+++ src/Containers/SpellsContainer/SpellsContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import  NavBar from '../NavBar/NavBar';
 import SpellBook from '../SpellBook/SpellBook';
 import SpellsForm from '../SpellsForm/SpellsForm';
@@ -6,24 +6,43 @@ import SpellsFilter from '../SpellsFilter/SpellsFilter';
 import './SpellsContainer.css';
 import { connect } from 'react-redux';
 
-export const SpellsContainer = ({selectedSpell}) => { 
-  const completeSpells = selectedSpell.map((spell) => {
-      return <SpellBook key={spell.id} {...spell}/>
-  })
-    return ( 
-        <section className='spells-container'>
+
+export class SpellsContainer extends Component {
+  
+  filterSpells = (spell) => {
+
+    const { searchedSpell, spellBook } = this.props;
+     
+    let chosenOne = spellBook.find((spell) => {
+      let lowerCaseSpells = spell.spell.toLowerCase();
+      return lowerCaseSpells.includes(spell.toLowerCase());
+    });
+
+   searchedSpell([chosenOne]);
+
+  }
+
+  render () {
+    const { selectedSpell } = this.props;
+    const completeSpells = selectedSpell.map((spell) => {
+            return <SpellBook key={spell.id} {...spell}/>
+        })
+    return (
+      <section className='spells-container'>
            <NavBar />
-           <SpellsForm />
-           <SpellsFilter />
-           <div className='completed-spells'>
-             {completeSpells} 
-           </div>
+            <SpellsForm />
+            <SpellsFilter />
+            <div className='completed-spells'>
+              {completeSpells} 
+            </div>
         </section> 
     )
+  }
 }
 
-export const mapStateToProps = ({ selectedSpell }) => ({
-    selectedSpell
+export const mapStateToProps = ({ selectedSpell, spellBook }) => ({
+    selectedSpell,
+    spellBook
 })
 
 export default connect(mapStateToProps)(SpellsContainer);
