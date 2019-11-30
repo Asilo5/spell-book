09ef6868defc439f5dcd96e1a38cb7813998a1cf
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-29 20:57:36 -0700

    Change App component from Class to functional as well

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import HouseForm  from '../../Containers/HouseForm/HouseForm';
 import  SpellsContainer  from '../../Containers/SpellsContainer/SpellsContainer';
 import FavouritesContainer from '../../Containers/FavouritesContainer/FavouritesContainer';
 import { Route } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-
-  render() {
+const App = () => {
     return (
       <section>
         <Route exact path='/' render={() => <HouseForm /> } />
@@ -15,7 +13,6 @@ class App extends Component {
         <Route exact path='/favourites' render={() => <FavouritesContainer /> } />
       </section>
     );
-  }
 }
 
 export default App;
