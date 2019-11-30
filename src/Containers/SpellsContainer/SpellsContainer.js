9a97fb396f226cb678e0d84e9d3725a347ba47fc
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-29 18:34:19 -0700

    Add buttons to to SpellsFilter

--- src/Containers/SpellsContainer/SpellsContainer.js
+++ src/Containers/SpellsContainer/SpellsContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import  NavBar from '../NavBar/NavBar';
 import SpellBook from '../SpellBook/SpellBook';
 import SpellsForm from '../SpellsForm/SpellsForm';
+import SpellsFilter from '../SpellsFilter/SpellsFilter';
 import './SpellsContainer.css';
 import { connect } from 'react-redux';
 
@@ -13,6 +14,7 @@ export const SpellsContainer = ({selectedSpell}) => {
         <section className='spells-container'>
            <NavBar />
            <SpellsForm />
+           <SpellsFilter />
            <div className='completed-spells'>
              {completeSpells} 
            </div>
---
+++ src/Containers/SpellsFilter/SpellsFilter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SpellsFilter.css';
+
+const SpellsFilter = () => {
+    return (
+        <section>
+            <button>Charm</button>
+            <button>Enchantment</button>
+            <button>Hex</button>
+            <button>Spell</button>
+            <button>Curse</button>
+        </section>
+    )
+}
+
+export default SpellsFilter;
