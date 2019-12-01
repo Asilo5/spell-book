878e29c933182ec84ab2db4c1f2c9f9578e2b714
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-01 10:12:18 -0700

    Updates

---
+++ precache-manifest.d4b677873bc1668f48a909bb4a71dfbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c4837efda94fb36aa7a734f154e89b",
+    "url": "/spell-book/index.html"
+  },
+  {
+    "revision": "41d422e9b33b4a87f931",
+    "url": "/spell-book/static/css/main.be83ba86.chunk.css"
+  },
+  {
+    "revision": "fc541d3770f2faa48562",
+    "url": "/spell-book/static/js/2.1c8d6b52.chunk.js"
+  },
+  {
+    "revision": "41d422e9b33b4a87f931",
+    "url": "/spell-book/static/js/main.64ad46cb.chunk.js"
+  },
+  {
+    "revision": "f068b0604a2be923a3e3",
+    "url": "/spell-book/static/js/runtime-main.e7b041e3.js"
+  }
+]);
